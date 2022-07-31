57dbf8d2e24e2b8c0dcd75cede94073b51cf2af0
talosrobert <29042337+talosrobert@users.noreply.github.com>
2022-07-31 20:22:36 +0200

    Chaper 4.5 unfinished

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Go Practice Repository
 ## build and run
 1. make dev
 2. ./build/main -cfg cmd/conf.json >> build/info.log 2>> build/err.log
+
+
+PAGE 141
--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -11,10 +11,24 @@ type SnippetModel struct {
 }
 
 func (sm *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	query := `INSERT INTO snippets (title, content, created, expires)
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := sm.DB.Exec(query, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (sm *SnippetModel) Get(id int) (*models.Snippet, error) {
+
 	return nil, nil
 }
 
